68860dcc3350622ba88b2afb88585f66361d4553
Juan Pablo Pérez Lantes <jplantes@gmail.com>
2023-10-24 18:37:39 -0300

    Patron adaptador

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -6,16 +6,19 @@ import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 import { PokeResults } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ){}
 
-  private readonly axios: AxiosInstance = axios;
+  
 
 
 
@@ -23,7 +26,7 @@ export class SeedService {
   async correrSeed() {
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeResults>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await this.http.get<PokeResults>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     const pokemonToInsert: { name: string, no: number }[] = [];
 
